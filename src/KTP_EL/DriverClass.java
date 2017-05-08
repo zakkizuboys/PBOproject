b98fd0f12b11abe59e0f4d788b4cf34c12dfe7d9
zakkizuboys <zakkizuboys@zakkizuboys>
2017-05-08 13:56:45 +0700

    Add script

--- src/KTP_EL/DriverClass.java
+++ src/KTP_EL/DriverClass.java
@@ -13,8 +13,9 @@ import java.util.Scanner;
  */
 public class DriverClass {
     public static void main(String[] args) {
-        int c;
+        int c,d;
         Scanner sc = new Scanner(System.in);
+        setGet st = new setGet();
         
         System.out.println("1. kelurahan bisa");
         System.out.println("2. kelurahan siap");
@@ -22,6 +23,7 @@ public class DriverClass {
         c = sc.nextInt();
         switch(c){
             case 1:
+                st.setKelurahan("biasa");
                 System.out.println("pilih RT");
                 System.out.println("1. RT 01");
                 System.out.println("2. RT 02");
@@ -29,8 +31,25 @@ public class DriverClass {
                 System.out.println("4. RT 04");
                 System.out.println("5. RT 05");
                 System.out.println("masukkan RT : ");
+                d= sc.nextInt();
+                
+                switch(d){
+                    case 1:
+                        st.setRT("01");
+                        System.out.println("masukkan nama :");
+                        System.out.println("masukkan agama :");
+                        System.out.println("masukkan jenis kelamin :");
+                        System.out.println("masukkan pekerjaan :");
+                        System.out.println("masukkan golongan darah :");
+                        System.out.println("masukkan tempat lahir :");
+                        System.out.println("masukkan tanggal lahir :");
+                        System.out.println("masukkan kewarganegaraan :");
+                        break;
+                }
+                
                 break;
             case 2:
+                st.setKelurahan("siap");
                 System.out.println("pilih RT");
                 System.out.println("1. RT 11");
                 System.out.println("2. RT 12");
@@ -38,6 +57,8 @@ public class DriverClass {
                 System.out.println("4. RT 14");
                 System.out.println("5. RT 15");
                 System.out.println("masukkan RT : ");
+                d= sc.nextInt();
+                break;
         }
     }
 }
